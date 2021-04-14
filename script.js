409a1c052df2d0de42352804fbb25310a216104b
Luis Starlino <luis.guilherme009@gmail.com>
2021-04-14 08:33:48 -0300

    adding modification

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function insetUsers() {
-    
+/*function insetUsers() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(dados => console.log(dados));
+}*/
+
+async function insertUsers(){
+    let response = await fetch('https://jsonplaceholder.typicode.com/users');
+    let dados = await response.json();
+    console.log(dados);
 }
